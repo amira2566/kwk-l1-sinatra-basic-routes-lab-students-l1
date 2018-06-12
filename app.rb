3f3a3630f91cccfc83db79e4611f78c63babf03f
amira2566 <braemira2@icloud.com>
2018-06-12 21:31:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+  get "/name" do 
+    "My name is Amira"
+  end
+  get "/hometown" do
+    "My hometown is Huntley, Illinois"
+  end
+  get "/favorite-song" do
+    "My favorite song is Bartier Cardi"
+  end
 end
